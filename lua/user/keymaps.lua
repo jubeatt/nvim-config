238d963c2b3707bd2a0e0f98f45899027192272c
PeaNu <jimdevelopesite@gmail.com>
2022-10-23 15:21:44 +0800

    change keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -35,6 +35,7 @@ keymap("n", "<S-Right>", ":vertical resize -2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<S-w>", ":Bdelete<cr>", opts)
+keymap("n", "<S-q>", ":Bdelete!<cr>", opts) -- (Force)
 
 -- Move text up and down
 keymap("n", "<S-k>", ":m -2<CR>", opts)
